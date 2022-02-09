135958c902f15fc7438467a9f509be8e2b4945dd
Ian S <iansilber32@gmail.com>
2022-02-09 12:52:46 -0500

    learning some rspec testing

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,1 +1,14 @@
 # main file
+
+class World
+  attr_accessor :rows, :cols, :cell_grid
+  def initialize(rows=3, cols=3)
+    @rows = rows
+    @cols = cols
+
+    @cell_grid = Array.new(rows) do |_row|
+      Array.new(cols) do |col|
+      end
+    end
+  end
+end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -1,7 +1,20 @@
 # testing file
-
 require 'rspec'
+require_relative 'game_of_life.rb'
 
 describe 'Game of life' do
-  
+  context 'World' do
+    subject { World.new }
+    it 'should create a new world object' do
+      expect(subject.is_a?(World)).to be true
+    end
+    it 'should respond to proper methods' do
+      expect(subject).to respond_to(:rows)
+      expect(subject).to respond_to(:cols)
+      expect(subject).to respond_to(:cell_grid)
+    end
+    it 'should create a cell grid on initialization' do
+      expect(subject.cell_grid.is_a?(Array)).to be true
+    end
+  end
 end
